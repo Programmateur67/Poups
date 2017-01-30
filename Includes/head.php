e3921eb086a54d85c691ae7056a53fb6943d4379
RedMeca <tytydodo67140@gmail.com>
2017-01-30 10:36:00 +0100

    Page de connexion PHASE 1

---
+++ Includes/head.php
@@ -0,0 +1,10 @@
+<head>
+        <meta charset="utf-8"/>
+        <meta name="author" content="Tino Dorn"/>
+        <meta name="application-name" content="Poups"/>
+        <meta name="description" content="Une petite app pour la famille... Domotique, Gestion et le tout en quelques clics"/>
+        <link href="Extensions/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="Extensions/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+        <link href="main_style.css" rel="stylesheet"/>
+        <title>POUPS</title>
+</head>
