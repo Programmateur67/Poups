dbef43884669cc71d24dc0c3cb6a43dd3971c878
RedMeca <tytydodo67140@gmail.com>
2017-02-01 10:40:45 +0100

    Avancements dans l'accueil

--- Includes/head2.php
+++ Includes/head2.php
@@ -5,6 +5,7 @@
         <meta name="description" content="Une petite app pour la famille... Domotique, Gestion et le tout en quelques clics"/>
         <link href="../Extensions/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="../Extensions/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+        <link href="../Extensions/awesome-bootstrap-checkbox-master/awesome-bootstrap-checkbox.css" rel="stylesheet"/>
         <link href="../main_style.css" rel="stylesheet"/>
         <title>Poups</title>
 </head>
