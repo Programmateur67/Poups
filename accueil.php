ed9d9a82ba28517ce1bf16fbd341ac97d692148b
RedMeca <tytydodo67140@gmail.com>
2017-02-03 12:18:14 +0100

    Suppr awesome bootsrtap checkbox
    Style changes

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <?php include 'Includes/head.php' ?>
+    <body class="container background">
+        <div id="wrap">
+            <div class="well col-sm-offset-8 col-sm-4 margin-top-form"></div>
+        </div>
+        
+        <div class="row">
+            <div class="navbar navbar-fixed-bottom navbar-default navbar-special col-sm-5">
+                
+            </div>
+            <div class="divbtn navbar-fixed-bottom col-sm-offset-5 col-sm-2">
+                <a class="form-control text-center btn btn-default btn-black">10 H 20</a>
+            </div>
+            <div class="navbar navbar-fixed-bottom navbar-default navbar-special col-sm-offset-7 col-sm-5">
+            
+            </div>
+        </div>
+    </body>
+    <?php include 'Includes/scripts.php'?>
+</html>
